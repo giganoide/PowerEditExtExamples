1d06910025337425cf76331299a270545cf8ffdf
lponticelli <lorenzo.ponticelli@gmail.com>
2019-10-10 12:34:36 +0200

    Nuovi esempi per primi passi

--- 015_DocumentManagement/MyDocManagementAddin.cs
+++ 015_DocumentManagement/MyDocManagementAddin.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Atys.PowerEDIT;
 using Atys.PowerEDIT.Extensibility;
 
@@ -100,6 +101,62 @@ namespace TeamSystem.Customizations
             this._AddinState = ExtensionState.Running;
 
             //do something...
+
+            this._PowerEDITApp.ActiveDocumentChanged += this._PowerEDITApp_ActiveDocumentChanged;
+
+            this._PowerEDITApp.DocumentLoaded += this._PowerEDITApp_DocumentLoaded;
+            this._PowerEDITApp.DocumentCreated += this._PowerEDITApp_DocumentCreated;
+            this._PowerEDITApp.ClosingDocument += this._PowerEDITApp_ClosingDocument;
+        }
+
+        private void _PowerEDITApp_DocumentCreated(object sender, DocumentDataEventArgs e)
+        {
+            if (!e.IsPWEDoc)
+                return;
+
+            var pweDoc = (IPWEDoc) e.Document;
+
+            pweDoc.TextChanging += this.PweDoc_TextChanging;
+        }
+
+        private void _PowerEDITApp_DocumentLoaded(object sender, DocumentDataEventArgs e)
+        {
+            if (!e.IsPWEDoc)
+                return;
+
+            var pweDoc = (IPWEDoc)e.Document;
+
+            pweDoc.TextChanging += this.PweDoc_TextChanging;
+        }
+
+        private void _PowerEDITApp_ClosingDocument(object sender, DocumentDataEventArgs e)
+        {
+            if (!e.IsPWEDoc)
+                return;
+
+            var pweDoc = (IPWEDoc)e.Document;
+            try
+            {
+                pweDoc.TextChanging -= this.PweDoc_TextChanging;
+            }
+            catch (Exception )
+            {
+            }
+        }
+
+        private void PweDoc_TextChanging(object sender, TextChangeCancelEventArgs e)
+        {
+            if (e.LinesDeleted > 0 &&  e.DeletedText.Contains("gigio"))
+            {
+               
+                e.Cancel = true;
+            }
+        }
+
+        private void _PowerEDITApp_ActiveDocumentChanged(object sender, ActiveDocumentChangedEventArgs e)
+        {
+            //aggancio e sgancio da eventi documento attivo in continuo
+            //con stesso codice sopra
         }
 
         public void Shutdown()
@@ -132,26 +189,80 @@ namespace TeamSystem.Customizations
         {
             Debug.Assert(menuItem != null);
 
+            //var x = this._PowerEDITApp.OpenPWEDoc(@"");
+
             if (menuItem.Caption == MYCOMMAND1BUTTONCAPTION)
             {
-                var activeDoc = this._PowerEDITApp.GetTextActiveDoc();
+
+                var activeDoc = this._PowerEDITApp.GetPWEActiveDoc();
+                if (activeDoc == null)
+                {
+                    MessageBox.Show("Documento attivo non è di tipo testo");
+                    return;
+                }
 
                 var profileName = this._PowerEDITApp.GetActiveProfileName();
                 if (profileName != "PWEBase")
                     return;
-                
+
+                const string stringToReplace = "S";
+                const string replaceWith = "Raggio";
+
+                //ricerca e sostituzione testo MODO 1 (Manuale)
                 var countDoc = activeDoc.LineCount;
-                for (var i = 1; i < countDoc; i++)
+                for (var i = 1; i <= countDoc; i++)
                 {
                     var line = activeDoc.GetLine(i);
 
-                    var stringToReplace = "S";
-                    var replaceWith = "Raggio";
 
                     var newLine = line.Text.Replace(stringToReplace, replaceWith);
                     line.ReplaceAllText(newLine);
                 }
+
+                //ricerca e sostituzione testo MODO 2 (API) - solo find
+
+                var findResults = activeDoc.FindText(stringToReplace, SearchAction.SearchAll, SearchScope.All,
+                                                     SearchType.Standard, SearchCasing.Any,
+                                                     SearchWord.AnyText, string.Empty, true,
+                                                     UiInteraction.None);
+
+                //ricerca e sostituzione testo MODO 3 (API) - replace diretto
+                var replaceResults = activeDoc.ReplaceText(stringToReplace, replaceWith,
+                                                           SearchAction.SearchAll, SearchScope.All,
+                                                           SearchType.Standard, SearchCasing.Any,
+                                                           SearchWord.AnyText, string.Empty,
+                                                           UiInteraction.None);
+
+                //muoversi tra le righe
+                activeDoc.MoveCursor(MoveCursorTarget.ToDocumentBegin);
+                activeDoc.MoveCursor(MoveCursorTarget.PageDown);
+                activeDoc.MoveCursor(MoveCursorTarget.PageUp);
+                
+                //activeDoc.GetCurrentLine()
+                 activeDoc.SetCursorPosition(1, 1);
+                for (var i = 1; i <= countDoc; i++)
+                {
+                    activeDoc.SetCursorPosition(i, 1);
+                    var line = activeDoc.GetLine(i);
+                    //...
+
+                    activeDoc.SetSelection(line.TextRange);
+
+                }
+
+                //aggiunta dati
+                activeDoc.MoveCursor(MoveCursorTarget.ToDocumentEnd);
+                activeDoc.AddLine("nuova riga");
+                //activeDoc.AddLineBefore();
             }
+
+
+
+            //creazione nuovo documento e salvataggio
+            var newDoc = this._PowerEDITApp.CreatePWEDoc();
+            newDoc.AddLine("Prova su nuovo documento");
+            newDoc.SaveDocAs(@"");
+            newDoc.Activate();
 
         }
 
